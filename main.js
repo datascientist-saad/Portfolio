40af6c1b5d0c56e92a0f8a8094d07d7051358aa8
Muhammad Saad <muhdsaad107@gmail.com>
2021-06-01 14:20:17 +0500

    Added feedback

--- main.js
+++ main.js
@@ -32,3 +32,11 @@ navLinks.forEach(link => {
         })
     })
 })
+
+document.querySelectorAll('.feedback li').forEach(entry => entry.addEventListener('click', e => {
+    if(!entry.classList.contains('active')) {
+        document.querySelector('.feedback li.active').classList.remove('active');
+        entry.classList.add('active');
+    }
+    e.preventDefault();
+}));
